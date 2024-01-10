9a246349b928a0d51b71d3064c109ca3e13c0d87
Rosario Pulella <RosarioPulella@gmail.com>
2024-01-10 14:06:39 -0500

    Provide help, usage and argument parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,23 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import MyLib qualified (someFunc)
+
+import Options.Applicative
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
+  g <- execParser opts
+  putStrLn g
   MyLib.someFunc
+  where
+    opts = info (args <**> helper)
+      ( fullDesc
+      <> progDesc "Create a software release from a git repo using nix flakes"
+      <> header "flake-release-tool - create software releases"
+      )
+
+
+args :: Parser String
+args = strArgument
+  ( metavar "RELEASE_ID"
+  <> help "The Release Identifier" )
